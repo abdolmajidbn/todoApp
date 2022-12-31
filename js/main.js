f6fe081db79a06be5c6d70ba75c9b80cc90fd5a7
abdolmajidbn <60983994+abdolmajidbn@users.noreply.github.com>
2023-01-01 00:20:20 +0330

    edit task #1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 //TODO: get elmant
 const add_task_btn = document.querySelector("#add_task_btn");
 const remove_task_btn = document.querySelectorAll(".remove_task_btn");
-const add_task_label = document.querySelector("#add_task_label");
+const add_task_input = document.querySelector("#add_task_input");
+const edit_task_modal = document.querySelector(".edit_task_modal");
 let todo = document.querySelector(".todo");
 
 //add new task
 add_task_btn.addEventListener("click", () => {
-  if (add_task_label.value != "") {
-    todo.innerHTML += task_format(add_task_label.value);
-    add_task_label.value = "";
+  if (add_task_input.value != "") {
+    todo.innerHTML += task_format(add_task_input.value);
+    add_task_input.value = "";
   }
 });
 
-add_task_label.addEventListener("keypress", (e) => {
+add_task_input.addEventListener("keypress", (e) => {
   if (e.key == "Enter") {
     add_task_btn.click();
   }
@@ -20,47 +21,59 @@ add_task_label.addEventListener("keypress", (e) => {
 
 function task_format(task_name) {
   return `
-  <div class="task">
-  <div class="checkbox">
-    <input type="checkbox" />
-  </div>
-  <h4 class="task_name text-secondary">
-    ${task_name}
-  </h4>
-  <button
-    onclick="remove_task(this)"
-    class="remove_task_btn btn btn-outline-danger"
-  >
-    <i class="bi bi-trash"></i>
-  </button>
-</div>
+        <div class="task">
+        <div class="checkbox">
+          <input type="checkbox" />
+        </div>
+        <h4 class="task_name text-secondary">
+          ${task_name}
+        </h4>
+        <div class="task_buttons">
+          <button
+            class="edit_task_btn btn btn-outline-secondary"
+            onclick="edit_task(this)"
+          >
+            <i class="bi bi-pencil-fill"></i>
+          </button>
+          <button
+            onclick="remove_task(this)"
+            class="remove_task_btn btn btn-outline-danger"
+          >
+            <i class="bi bi-trash"></i>
+          </button>
+        </div>
+      </div>
 `;
 }
 //end add task
 //remove task
 
 function remove_task(e) {
-  e.parentElement.remove();
+  e.parentElement.parentElement.remove();
 }
 // end remove task
 //edit task
 
 function edit_task(e) {
-  let paragraph = document.querySelector(".task_name");
-  // const end_button = document.getElementById("end-editing");
-
-  paragraph.contentEditable = true;
-  paragraph.click;
-  // paragraph.style.backgroundColor = "#dddbdb";
+  let paragraph = e.parentElement.parentElement.querySelector(".task_name");
+  let edit_input = edit_task_modal.querySelector(".edit_input");
+  let save_btn = edit_task_modal.querySelector(".save");
+  let close_btn = edit_task_modal.querySelector(".close");
+  let cancel_btn = edit_task_modal.querySelector(".cancel");
+  edit_task_modal.style.display = "flex";
+  edit_input.value = paragraph.innerHTML;
+  save_btn.addEventListener("click", () => {
+    paragraph.innerHTML = edit_input.value;
+    close_btn.click();
+  });
+  close_btn.addEventListener("click", () => {
+    console.log(edit_input.value);
+    edit_task_modal.style.display = "none";
+  });
+  cancel_btn.addEventListener("click", () => {
+    close_btn.click();
+  });
+  console.log(e.parentElement.parentElement);
 }
 
-// edit_button.addEventListener("click", function () {
-//   paragraph.contentEditable = true;
-//   paragraph.style.backgroundColor = "#dddbdb";
-// });
-
-// end_button.addEventListener("click", function () {
-//   paragraph.contentEditable = false;
-//   paragraph.style.backgroundColor = "#ffe44d";
-// });
 // end edit task
